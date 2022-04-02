4afe16554ac73b72a4a680da7087379dba2810c5
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2022-04-02 07:53:27 +0530

    Time: 56 ms (90.74%), Space: 19.4 MB (95.14%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    bool isp(string s){
-        int i=0,j=s.size()-1;
+    bool isp(string& s,int i,int j){
         while(i<j){
             if(s[i]!=s[j])
                 return 0;
@@ -10,7 +9,7 @@ public:
         return 1;
     }
     bool validPalindrome(string s) {
-        if(isp(s))
+        if(isp(s,0,s.size()-1))
             return true;
         
         int i=0,j=s.size()-1;
@@ -18,7 +17,7 @@ public:
             i++,j--;
         }
         
-        if(isp(s.substr(i,j-i))||isp(s.substr(i+1,j-i)))
+        if(isp(s,i,j-1)||isp(s,i+1,j))
             return true;
         return false;
     }
