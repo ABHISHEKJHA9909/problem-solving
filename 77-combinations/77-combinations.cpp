12f0e1867d5024d6d0ea51855e1d2f4b24af6a7e
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2022-04-07 22:53:30 +0530

    Time: 105 ms (24.57%), Space: 11.7 MB (39.79%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -13,7 +13,24 @@ public:
         }
     }
     vector<vector<int>> combine(int n, int k) {
-        rec(1,k,n,{});
+        int x=(1<<n);
+        
+        for(int i=0;i<x;i++){
+            int cnt=0;
+            for(int j=0;j<n;j++){
+                if(i&(1<<j))
+                    cnt++;
+            }
+            if(cnt==k){
+                vector<int>curr;
+                for(int j=0;j<n;j++){
+                    if(i&(1<<j))
+                        curr.push_back(j+1);
+                }
+                res.push_back(curr);
+            }
+        }
+        
         return res;
     }
 };
