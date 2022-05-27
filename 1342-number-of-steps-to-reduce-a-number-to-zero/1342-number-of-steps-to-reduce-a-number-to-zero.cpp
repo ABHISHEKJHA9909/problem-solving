d25715a1a025cf66c395d82367140caba87f5c56
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2022-05-27 19:15:51 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (26.66%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -3,8 +3,8 @@ public:
     int numberOfSteps(int num) {
         if(num==0)
             return 0;
-        if(num&1)
-            return 1 + numberOfSteps(num-1);
-        return 1 + numberOfSteps(num/2);
+        if(num & 1)
+            return 1+numberOfSteps(num-1);
+        return 1 + numberOfSteps(num>>1);
     }
 };
