549df1f38a2e592a7be4d1e114c53834140165e1
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2022-06-22 12:40:32 +0530

    Time: 17 ms (43.91%), Space: 49.3 MB (5.85%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,13 +1,32 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> m =new HashMap<Integer,Integer>();
+        
+        HashMap<Integer,ArrayList<Integer>> m =new HashMap<Integer,ArrayList<Integer>>();
         
         for(int i=0;i<nums.length;i++){
-            if(m.containsKey(target-nums[i])){
-                int arr[]= {m.get(target-nums[i]),i};
+            if(m.containsKey(nums[i])==false){
+                m.put(nums[i],new ArrayList<Integer>());
+            }
+            m.get(nums[i]).add(i);
+        }
+        
+        Arrays.sort(nums);
+        int i=0,j=nums.length-1;
+        while(i<j){
+            if(nums[i]+nums[j]==target){
+                if(nums[i]==nums[j]){
+                    int arr[]={m.get(nums[i]).get(0),m.get(nums[j]).get(1)};
+                    return arr;
+                }
+                int arr[]={m.get(nums[i]).get(0),m.get(nums[j]).get(0)};
                 return arr;
             }
-            m.put(nums[i],i);
+            else if(nums[i]+nums[j]<target){
+                i++;
+            }
+            else{
+                j--;
+            }
         }
         int arr[]={0};
         return arr;
