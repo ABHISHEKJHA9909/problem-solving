534643ee21a378c74e77e38e5f13c69e0bb2d686
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2022-06-27 17:49:48 +0530

    Time: 1 ms (94.46%), Space: 53.6 MB (62.87%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -1,30 +1,15 @@
 class Solution {
     public boolean checkPossibility(int[] nums) {
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]>nums[i+1]){
-                int temp=nums[i+1];
-                nums[i+1]=nums[i];
-                
-                int cnt=0;
-                for(int j=0;j<nums.length-1;j++){
-                    if(nums[j]>nums[j+1]){
-                        cnt++;
-                    }
-                }
-                
-                nums[i]=temp;
-                nums[i+1]=temp;
-                int cnt1=0;
-                for(int j=0;j<nums.length-1;j++){
-                    if(nums[j]>nums[j+1]){
-                        cnt1++;
-                    }
-                }
-                if(cnt==0||cnt1==0)
-                    return true;
-                return false;
+        int cnt=0;
+        for(int i=1;i<nums.length && cnt<=1;i++){
+            if(nums[i-1]>nums[i]){
+                if(i-2<0 || nums[i-2]<=nums[i])
+                    nums[i-1]=nums[i];
+                else
+                    nums[i]=nums[i-1];
+                cnt++;
             }
         }
-        return true;
+        return cnt<=1;
     }
 }
