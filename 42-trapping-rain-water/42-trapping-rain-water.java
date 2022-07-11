137c4d30a5725c1291d4f0fab65d48dd0b8b265e
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2022-07-11 14:53:04 +0530

    Time: 2 ms (57.31%), Space: 48.7 MB (48.95%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,30 +1,20 @@
 class Solution {
     public int trap(int[] arr) {
         int n=arr.length;
+        int i=0,j=n-1,im=arr[i],jm=arr[j],res=0;
         
-        int l[]=new int[n];
-        int r[]=new int[n];
-        for(int i=0,curr=arr[i];i<n;i++){
-            curr=Math.max(curr,arr[i]);
-            l[i]=curr;
+        while(i<j){
+            im=Math.max(im,arr[i]);
+            jm=Math.max(jm,arr[j]);
+            
+            res+=Math.min(im,jm)-Math.min(arr[i],arr[j]);
+            
+            if(arr[i]<arr[j])
+                i++;
+            else
+                j--;
         }
-        for(int j=n-1,curr=arr[j];j>=0;j--){
-            curr=Math.max(curr,arr[j]);
-            r[j]=curr;
-        }
-        int res=0;
-        // for(int i:arr)
-        //     System.out.print(i+" ");
-        // System.out.println();
-        // for(int i:l)
-        //     System.out.print(i+" ");
-        // System.out.println();
-        // for(int i:r)
-        //     System.out.print(i+" ");
         
-        for(int i=0;i<n;i++){
-            res+=Math.min(l[i],r[i])-arr[i];
-        }
         return res;
     }
 }
