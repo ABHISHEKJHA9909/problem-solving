d3fbc05791b04a5e46cd23dde3a78dd7da330ab6
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2022-08-31 03:03:47 +0530

    Time: 297 ms (25.06%), Space: 24.7 MB (32.06%) - LeetHub

--- 407-trapping-rain-water-ii/407-trapping-rain-water-ii.cpp
+++ 407-trapping-rain-water-ii/407-trapping-rain-water-ii.cpp
@@ -7,22 +7,23 @@ public:
         int r=arr.size(),c=arr[0].size();
         priority_queue<vector<int>>pq;
         
-        vector<vector<int>>vis(r,vector<int>(c,0));
+        bool vis[r][c];
+        for(auto &i:vis)for(auto &j:i)j=0;
         
         for(int i=0;i<r;i++){
-            vis[i][0]=vis[i][c-1]=1;
+            vis[i][0]=vis[i][c-1]=true;
             pq.push({-arr[i][0],i,0});
             pq.push({-arr[i][c-1],i,c-1});
         }
         
         for(int j=0;j<c;j++){
-            vis[0][j]=vis[r-1][j]=1;
+            vis[0][j]=vis[r-1][j]=true;
             pq.push({-arr[0][j],0,j});
             pq.push({-arr[r-1][j],r-1,j});
         }
         
         int dirs[4][2]={{0,-1},{0,1},{1,0},{-1,0}};
-        int res=0;
+        int res=0,i,j;
         
         while(pq.size()){
             vector<int> curr=pq.top();
@@ -30,11 +31,11 @@ public:
             int h=-curr[0];
             
             for(auto &dir:dirs){
-                int i=curr[1]+dir[0],j=curr[2]+dir[1];
+                i=curr[1]+dir[0],j=curr[2]+dir[1];
                 if(i<0||i>=r||j<0||j>=c)
                     continue;
-                if(vis[i][j]==0){
-                    vis[i][j]=1;
+                if(vis[i][j]==false){
+                    vis[i][j]=true;
                     if(arr[i][j]<h)
                         res+=(h-arr[i][j]);
                     pq.push({-max(arr[i][j],h),i,j});
