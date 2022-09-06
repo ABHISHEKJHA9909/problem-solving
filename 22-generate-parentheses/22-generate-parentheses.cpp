ba09f2d26d9971c25ebcdd902cd763b0aaf95fc4
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2022-09-06 12:35:50 +0530

    Time: 8 ms (45.61%), Space: 13.7 MB (59.97%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
-    bool valid(string &s){
-        stack<char>st;
-        for(char c:s){
-            if(c=='(')
-                st.push(')');
-            else{
-                if(st.size()==0)
-                    return false;
-                st.pop();
-            }
-        }
-        return st.size()==0;
-    }
-    
-    void generateall(int n,string s,vector<string> &res){
-        if(n==0){
-            if(valid(s))
-                res.push_back(s);
+    void solve(int i,int j,string s,vector<string> &res){
+        if(j==0){
+            res.push_back(s);
             return;
         }
-        generateall(n-1,s+"(",res);
-        generateall(n-1,s+")",res);
+        if(i>0)
+            solve(i-1,j,s+'(',res);
+        if(j>i)
+            solve(i,j-1,s+')',res);
     }
-    
     vector<string> generateParenthesis(int n) {
-        vector<string>res;
-        generateall(2*n,"",res);
+        vector<string>res; 
+        solve(n,n,"",res);
         return res;
     }
 };
