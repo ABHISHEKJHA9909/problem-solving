30bf8de710f8bb2a71ce3cc90ff7910ca9922a25
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2022-09-21 04:28:34 +0530

    Time: 3 ms (64.52%), Space: 7 MB (73.65%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -4,14 +4,14 @@ public:
         int n=arr.size();
         
         for(int i=0;i<n;i++){
-            for(int j=0;j<n-i;j++){
-                swap(arr[i][j],arr[n-j-1][n-i-1]);
+            for(int j=0;j<=i;j++){
+                swap(arr[i][j],arr[j][i]);
             }
         }
         
-        for(int j=0;j<n;j++){
-            for(int i=0;i<n/2;i++){
-                swap(arr[i][j],arr[n-i-1][j]);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                swap(arr[i][j],arr[i][n-j-1]);
             }
         }
         
