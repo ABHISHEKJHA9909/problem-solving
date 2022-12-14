963482e64758420e74b6f42266948a5f4a1eaf7d
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2022-12-14 10:59:12 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (50.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,15 +5,15 @@ public:
         
         if(n==1)
             return nums[0];
-        int arr[n][2];
-        arr[1][0]=nums[1];
-        arr[1][1]=nums[0];
+        
+        int with=nums[1],without=nums[0],temp;
         
         for(int i=2;i<n;i++){
-            arr[i][0]=nums[i]+arr[i-1][1];
-            arr[i][1]=max(arr[i-1][0],arr[i-1][1]);
+            temp=with;
+            with=nums[i]+without;
+            without=max(temp,without);
         }
         
-        return max(arr[n-1][0],arr[n-1][1]);
+        return max(with,without);
     }
 };
