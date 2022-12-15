b23ac30ab5bb4749cbdfd6c5f5f0b5aee399a53b
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2022-12-16 00:56:09 +0530

    Time: 12 ms (86.67%), Space: 7.3 MB (79.14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
-    int n,m;
-    map<vector<int>,int>mm;
     
-    int rec(string &s,string &t,int i,int j){
-        if(mm.find({i,j})!=mm.end())
-            return mm[{i,j}];
-        if(i==n)
-            return m-j;
-        if(j==m)
-            return n-i;
+    int minDistance(string s, string t) {
+        int n=s.size() , m=t.size();
+        if(n==0)
+            return m;
+        if(m==0)
+            return n;
+        int dp[n][m];
+        memset(dp,0,sizeof(dp));
         
-        if(s[i]==t[j])
-            return rec(s,t,i+1,j+1);
-        return mm[{i,j}]=1+min(rec(s,t,i+1,j+1),min(rec(s,t,i+1,j),rec(s,t,i,j+1)));
-    }
-    
-    int minDistance(string word1, string word2) {
-        n=word1.size() , m=word2.size();
-        return rec(word1,word2,0,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0){
+                    if(j!=0){
+                        dp[i][j]=dp[i][j-1]+((s[i]==t[j])?(dp[i][j-1]==j?0:1):1);
+                    }
+                    else if(i!=0){
+                        dp[i][j]=dp[i-1][j]+((s[i]==t[j])?(dp[i-1][j]==i?0:1):1);
+                    }
+                    else
+                        dp[i][j]=!(s[i]==t[j]);
+                    
+                }
+                else{
+                    if(s[i]!=t[j])
+                        dp[i][j]=1 + min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                    else
+                        dp[i][j]=dp[i-1][j-1];
+                }
+                // cout<<dp[i][j]<<' ';
+            }
+            // cout<<endl;
+        }
+        
+        return dp[n-1][m-1];
     }
 };
