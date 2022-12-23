c4d3ca0be107812c42e910a7bdb3913df023f772
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2022-12-23 10:36:41 +0530

    Time: 478 ms (5.03%), Space: 471.9 MB (5.04%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    map<pair<int,int>,int>m;
+    
+    int rec(vector<int>arr,int i,int curr){
+        if(i>=arr.size())
+            return 0;
+        if(m.find({i,curr})!=m.end())
+            return m[{i,curr}];
+        
+        if(curr>=arr[i])
+            return rec(arr,i+1,arr[i]);
+        
+        return m[{i,curr}] = max(rec(arr,i+1,curr),arr[i]-curr+rec(arr,i+2,i+2<arr.size()?arr[i+2]:0));
+        
+    }
+    
+    int maxProfit(vector<int>& prices) {
+        return rec(prices,0,prices[0]);
+    }
+};
