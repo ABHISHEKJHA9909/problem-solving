58117cc17820764e212622d2bb653655c79a16b4
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2023-02-17 15:49:05 +0530

    Time: 161 ms (59.63%), Space: 39.8 MB (94.81%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.java
@@ -0,0 +1,28 @@
+class Solution {
+    int target=0;
+    
+    boolean rec(int[] arr,int i,int []sum){
+        if(i==-1){
+            return true;
+        }
+        for(int j=0;j<4;j++){
+            if(sum[j]+arr[i]>target)
+                continue;
+            sum[j]+=arr[i];
+            if(rec(arr,i-1,sum))
+                return true;
+            sum[j]-=arr[i];
+        }
+        return false;
+    }
+    
+    public boolean makesquare(int[] arr) {
+        for(int i:arr)
+            target+=i;
+        if(target%4!=0)
+            return false;
+        target/=4;
+        Arrays.sort(arr);
+        return rec(arr,arr.length-1,new int[4]);
+    }
+}
