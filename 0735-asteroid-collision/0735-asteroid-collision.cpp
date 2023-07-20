6a9c4dfa2f0d8818f536e9182311eaa7c983ba74
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2023-07-20 10:24:07 +0530

    Time: 20 ms (29.55%), Space: 18 MB (8.30%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
+    vector<int> asteroidCollision(vector<int>& arr) {
         stack<int>s;
         
-        for(int &i:asteroids){
-            int curr=i;
-            
-            while( s.size() && curr && (s.top()>0 && curr<0) ){
-                // cout<<s.top()<<' ';
-                if(abs(curr)<abs(s.top()))
-                    curr=s.top();
-                else if(abs(curr)==abs(s.top()))
-                    curr=0;
-                s.pop();
+        for(int &i:arr){
+            if(s.size()){
+                while(s.size()){
+                    if(s.top()>0 && i<0){
+                        if(s.top()>-i)
+                            i=s.top();
+                        else if(s.top()==-i)
+                            i=0;
+                        s.pop();
+                    }
+                    else
+                        break;
+                    if(i==0)
+                        break;
+                }
             }
-            if(curr)
-                s.push(curr);
-            
+            if(i)
+                s.push(i);
         }
         
         vector<int>res;
