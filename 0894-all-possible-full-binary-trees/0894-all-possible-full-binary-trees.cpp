3d99483b8a4e7a60aca3f186a99d3c441aa2f4f7
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2023-07-23 22:21:13 +0530

    Time: 99 ms (77.03%), Space: 28.4 MB (61.48%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
@@ -11,17 +11,18 @@
  */
 class Solution {
 public:
+    // map<int,vector<TreeNode*>>m;
     
-    vector<TreeNode*> rec(int n){
+    vector<TreeNode*> rec(int n,unordered_map<int,vector<TreeNode*>>&m){
+        if(m.find(n)!=m.end())
+            return m[n];
         if(n==1)
-            return {new TreeNode(0)};
-            
-        
+            return m[n]={new TreeNode(0)};
         vector<TreeNode*>curr;
         
         for(int i=1;i<=n-2;i+=2){
-            vector<TreeNode*>left=rec(i);
-            vector<TreeNode*>right=rec(n-1-i);
+            vector<TreeNode*>left=rec(i,m);
+            vector<TreeNode*>right=rec(n-1-i,m);
             
             for(auto &l:left){
                 for(auto &r:right){
@@ -34,10 +35,11 @@ public:
         }
         
             
-        return curr;
+        return m[n]=curr;
     }
     
     vector<TreeNode*> allPossibleFBT(int n) {
-        return rec(n);
+        unordered_map<int,vector<TreeNode*>>mm;
+        return rec(n,mm);
     }
 };
