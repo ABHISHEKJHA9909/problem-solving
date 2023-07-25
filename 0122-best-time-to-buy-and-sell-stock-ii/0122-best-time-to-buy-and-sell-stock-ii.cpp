b3bda7fa2d477d4bebc4a876554527b151d7b171
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2023-07-25 10:08:18 +0530

    Time: 9 ms (58.71%), Space: 13 MB (79.13%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,7 +4,6 @@ public:
         int sell=0,buy=INT_MIN;
         
         for(int &i:prices){
-            int temp=buy;
             buy=max(buy,sell-i);
             sell=max(sell,buy+i);
         }
