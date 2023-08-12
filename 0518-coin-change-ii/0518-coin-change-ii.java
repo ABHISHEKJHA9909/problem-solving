21f8c84a1a6d853df68a9134438a643a428f6f6b
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2023-08-12 17:21:04 +0530

    Time: 9 ms (35.00%), Space: 45.7 MB (44.62%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -21,6 +21,35 @@ class Solution {
     }
     
     public int change(int amount, int[] coins) {
-        return rec(0,amount,0,coins);
+        int n=coins.length;
+        
+        int dp[][]=new int[n+1][amount+1];
+        
+        dp[0][0]=1;
+        
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=amount;j++){
+                if(i==0||j==0){
+                    if(i==0 && j==0){
+                        continue;
+                    }
+                    else if(i==0){
+                        dp[i][j]=0;
+                    }
+                    else{
+                        dp[i][j]=1;
+                    }
+                }
+                else{
+                    if(j>=coins[i-1]){
+                        dp[i][j]=dp[i][j-coins[i-1]] + dp[i-1][j];
+                    }
+                    else{
+                        dp[i][j]=dp[i-1][j];
+                    }
+                }
+            }
+        }
+        return dp[n][amount];
     }
 }
