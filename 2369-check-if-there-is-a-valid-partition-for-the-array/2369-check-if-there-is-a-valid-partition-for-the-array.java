f530daabc76e73582d7ad34a1877f42b43124c2a
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2023-08-13 08:17:52 +0530

    Time: 4 ms (100.00%), Space: 61.5 MB (35.92%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,23 +1,18 @@
 class Solution {
+    boolean rec(int i,int[] nums){
+        if(i==-1)
+            return true;
+        
+        if(i>0 && nums[i]==nums[i-1] && rec(i-2,nums))
+            return true;
+        else if(i>1 && nums[i]==nums[i-1] && nums[i-1]==nums[i-2] && rec(i-3,nums))
+            return true;
+        else if(i>1 && nums[i]==(nums[i-1]+1) && nums[i-1]==(nums[i-2]+1) && rec(i-3,nums))
+            return true;
+        return false;
+    }
+    
     public boolean validPartition(int[] nums) {
-        int len=nums.length;
-        int dp[]=new int[len+1];
-        
-        dp[0]=1;
-        if(nums[0]==nums[1]){
-            dp[2]=1;
-        }
-                
-        for(int i=2;i<len;i++){
-            int ind=i+1;
-            if(nums[i]==nums[i-1] && dp[ind-2]==1)
-                dp[ind]=dp[ind-2];
-            else if(nums[i]==nums[i-1] && nums[i-1]==nums[i-2] && dp[ind-3]==1)
-                dp[ind]=dp[ind-3];
-            else if(nums[i]==(nums[i-1]+1) && nums[i-1]==(nums[i-2]+1) && dp[ind-3]==1)
-                dp[ind]=dp[ind-3];
-        }
-        
-        return dp[len]==1;
+        return rec(nums.length-1,nums);
     }
 }
