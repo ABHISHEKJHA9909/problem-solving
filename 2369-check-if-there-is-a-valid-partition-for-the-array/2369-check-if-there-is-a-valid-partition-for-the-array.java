76f993b6d634680aeca2d83116f252f418ef1e7d
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2023-08-13 08:21:12 +0530

    Time: 15 ms (26.21%), Space: 67.2 MB (5.83%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,18 +1,24 @@
 class Solution {
-    boolean rec(int i,int[] nums){
+    boolean rec(int i,int[] nums,Map<Integer,Boolean> m){
         if(i==-1)
             return true;
         
-        if(i>0 && nums[i]==nums[i-1] && rec(i-2,nums))
+        if(m.get(i)!=null)
+            return m.get(i);
+        
+        m.put(i,true);
+        if(i>0 && nums[i]==nums[i-1] && rec(i-2,nums,m))
             return true;
-        else if(i>1 && nums[i]==nums[i-1] && nums[i-1]==nums[i-2] && rec(i-3,nums))
+        else if(i>1 && nums[i]==nums[i-1] && nums[i-1]==nums[i-2] && rec(i-3,nums,m))
             return true;
-        else if(i>1 && nums[i]==(nums[i-1]+1) && nums[i-1]==(nums[i-2]+1) && rec(i-3,nums))
+        else if(i>1 && nums[i]==(nums[i-1]+1) && nums[i-1]==(nums[i-2]+1) && rec(i-3,nums,m))
             return true;
+        m.put(i,false);
         return false;
     }
     
     public boolean validPartition(int[] nums) {
-        return rec(nums.length-1,nums);
+        Map<Integer,Boolean>m=new HashMap<Integer,Boolean>();
+        return rec(nums.length-1,nums,m);
     }
 }
