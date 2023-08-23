981518908f104abd5909caa77fe5a39269754a05
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2023-08-23 14:25:50 +0530

    Time: 7 ms (22.19%), Space: 42.1 MB (11.53%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,16 +1,16 @@
 class Solution {
     public String reorganizeString(String s) {
         
-        int cnt[]=new int[26];
+        int cnt[]=new int[256];
         
         int max=0;
         int c=0;
         
         for(int i=0;i<s.length();i++){
-            cnt[s.charAt(i)-'a']++;
-            if(cnt[s.charAt(i)-'a']>max){
-                max=cnt[s.charAt(i)-'a'];
-                c=s.charAt(i)-'a';
+            ++cnt[s.charAt(i)];
+            if(cnt[s.charAt(i)]>max){
+                max=cnt[s.charAt(i)];
+                c=s.charAt(i);
             }
         }
         
@@ -18,23 +18,23 @@ class Solution {
         if(max>(s.length()+1)/2)
             return "";
         
-        Character arr[]=new Character[s.length()];
+        int arr[]=new int[s.length()];
         
         int idx=0;
         while(cnt[c]>0){
-            arr[idx]=(char)('a'+c);
+            arr[idx]=c;
             idx+=2;
-            cnt[c]--;
+            --cnt[c];
         }
         
-        for(int i=0;i<26;i++){
+        for(int i=97;i<233;i++){
             while(cnt[i]>0){
                 if(idx>=s.length()){
                     idx=1;
                 }
-                arr[idx]=(char)('a'+i);
+                arr[idx]=i;
                 idx+=2;
-                cnt[i]--;
+                --cnt[i];
             }
         }
         
@@ -43,7 +43,7 @@ class Solution {
         String res="";
         
         for(int i=0;i<s.length();i++){
-            res+=arr[i];
+            res+=(char)arr[i];
         }
         
         return res;
