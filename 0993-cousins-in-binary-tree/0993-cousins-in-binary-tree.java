9a2d8e32c1bfe704d57c457750c6cb2c2c985fd9
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2023-08-25 14:34:31 +0530

    Time: 16 ms (6.23%), Space: 41 MB (6.76%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -14,38 +14,32 @@
  * }
  */
 class Solution {
-    private int dep_1;
-    private int dep_2;
-    private TreeNode par_1;
-    private TreeNode par_2;
+    private boolean answer;
     
-    void rec(TreeNode root,TreeNode par,int dep,int n1,int n2){
+    private int helper(TreeNode root,int x,int y){
         if(root==null)
-            return;
-        if(root.val==n1){
-            dep_1=dep;
-            par_1=par;
+            return -1;
+        if(root.val==x||root.val==y)
+            return 0;
+        
+        int l=helper(root.left,x,y);
+        int r=helper(root.right,x,y);
+        
+        System.out.println(root.val+" "+l+" "+r);
+        if(l>=1 && r>=1 &&  l==r && l!=0){
+            answer=true;
         }
-        if(root.val==n2){
-            dep_2=dep;
-            par_2=par;
-        }
-        rec(root.left,root,dep+1,n1,n2);
-        rec(root.right,root,dep+1,n1,n2);
+        if(l==-1 && r==-1)
+            return -1;
+        return Math.max(l,r)+1;
     }
     
+    
     public boolean isCousins(TreeNode root, int x, int y) {
+        answer=false;
         
-        dep_1=-1;
-        dep_2=-1;
-        par_1=null;
-        par_2=null;
+        helper(root,x,y);
         
-        rec(root,null,0,x,y);
-        
-        if(dep_1!=-1 && dep_2!=-1 && dep_1==dep_2 && par_1!=par_2){
-            return true;
-        }
-        return false;
+        return answer;
     }
 }
