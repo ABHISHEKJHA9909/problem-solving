50a61eefb0f8c56cd23ae996b1f8ffaf691e53ae
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2023-09-07 09:19:03 +0530

    Time: 5 ms (95.38%), Space: 41.1 MB (95.38%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -3,17 +3,20 @@ class Solution {
         int n=nums1.length;
         int m=nums2.length;
         
-        int dp[][]=new int[n+1][m+1];
-        // int res=
+        int dp[]=new int[m+1];
+        
         for(int i=1;i<=n;i++){
+            int prev=0;
             for(int j=1;j<=m;j++){
+                int temp=dp[j];
                 if(nums1[i-1]==nums2[j-1])
-                    dp[i][j]=dp[i-1][j-1]+1;
+                    dp[j]=prev+1;
                 else
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                    dp[j]=Math.max(dp[j],dp[j-1]);
+                prev=temp;
             }
         }
         
-        return dp[n][m];
+        return dp[m];
     }
 }
