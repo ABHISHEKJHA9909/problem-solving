53001492798f4ad3a52f83f1cfe6fb478ce93a6e
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2023-09-12 09:37:30 +0530

    Time: 14 ms (38.56%), Space: 44.1 MB (96.87%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,26 +1,32 @@
+
 class Solution {
-    String sort(String s){
-        char c[]=s.toCharArray();
-        Arrays.sort(c);
-        return String.valueOf(c);
-    }
     public int minDeletions(String s) {
-        Set<Integer>hs=new HashSet<Integer>();
-        s=sort(s);
-        int res=0;
-        for(int i=0;i<s.length();){
-            char curr=s.charAt(i);
-            int currl=0;
-            while(i<s.length() && s.charAt(i)==curr){
-                currl++;
-                i++;
-            }
-            while(hs.contains(currl)!=false && currl!=0){
-                currl--;
-                res++;
-            }
-            hs.add(currl);
+        
+        int char_count[]=new int[26];
+        
+        int mx=0;
+        
+        for(int i=0;i<s.length();i++){
+            ++char_count[s.charAt(i)-'a'];
+            mx=Math.max(mx,char_count[s.charAt(i)-'a']);
         }
+        
+        int cnt[]=new int[mx+1];
+        
+        for(int i=0;i<26;i++){
+            ++cnt[char_count[i]];
+        }
+        
+        int res=0;
+        
+        for(int i=mx;i>0;i--){
+            if(cnt[i]>1){
+                res+=cnt[i]-1;
+                cnt[i-1]+=(cnt[i]-1);
+            }
+        }
+        
         return res;
+        
     }
 }
