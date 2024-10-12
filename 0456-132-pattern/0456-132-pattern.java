1c3cbdf1f38aa12d9f2efb6f6218278e0af36307
ABHISHEKJHA9909 <88017516+ABHISHEKJHA9909@users.noreply.github.com>
2024-10-12 23:37:03 +0530

    Time: 51 ms (12.28%), Space: 65.4 MB (97.34%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -2,17 +2,17 @@ class Solution {
     public boolean find132pattern(int[] nums) {
         int len=nums.length;
         int[] min=new int[len];
-
-        for(int i=0,x=nums[0];i<len;++i){
-            if(nums[i]<x){
-                x=nums[i];
-            }
-            min[i]=x;
-        }
         
         Stack<Integer> s=new Stack<>();
         
-        for(int i=0;i<len;++i){
+        for(int i=0,x=nums[0];i<len;++i){
+            
+            if(nums[i]<x){
+                x=nums[i];
+            }
+            
+            min[i]=x;
+            
             while(!s.isEmpty() && nums[s.peek()] <= nums[i]){
                 s.pop();
             }
